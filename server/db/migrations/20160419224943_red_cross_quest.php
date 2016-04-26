6f2edc848d7c8696199067e2f7fe515a91252eed
Thomas Manson <dev.mansonthomas@gmail.com>
2016-04-27 01:12:57 +0200

    work on schema and fake data

--- server/db/migrations/20160419224943_red_cross_quest.php
+++ server/db/migrations/20160419224943_red_cross_quest.php
@@ -28,41 +28,41 @@ class RedCrossQuest extends AbstractMigration
     public function change()
     {
         $ul_table = $this->table('ul');
-        $ul_table ->addColumn('name'        , 'string')
-                  ->addColumn('phone'       , 'string')
-                  ->addColumn('longitude'   , 'string') //to send coordinates/addresse to people
-                  ->addColumn('latitude'    , 'string')
-                  ->addColumn('adresse'     , 'string')
-                  ->addColumn('codePostale' , 'string')
-                  ->addColumn('ville'       , 'string')
-                  ->addColumn('pegass_id'   , 'integer')
+        $ul_table ->addColumn('name'        , 'string' , array('limit' => 50))
+                  ->addColumn('phone'       , 'string' , array('limit' => 13))
+                  ->addColumn('latitude'    , 'decimal', array('precision' => 10, 'scale' => 6))
+                  ->addColumn('longitude'   , 'decimal', array('precision' => 10, 'scale' => 6))
+                  ->addColumn('address'     , 'string' , array('limit' => 70))
+                  ->addColumn('postal_code' , 'string' , array('limit' => 15))
+                  ->addColumn('city'        , 'string' , array('limit' => 70))
+                  ->addColumn('external_id' , 'integer')
                   ->create();
 
         $ul_table->insert([
-          ["name" => "N/A", "pegass_id" => 0],
-          ["name" => "Paris IV", "pegass_id" => 3]
-
+          ["name" => "Non Bénévole", "phone" => "N/A", "longitude" => "0", "latitude" => "0", "address" => "N/A","postal_code" => "N/A"  ,"city" => "N/A"  , "external_id" => 0],
+          ["name" => "Paris IV"    , "phone" => "N/A", "longitude" => "0", "latitude" => "0", "address" => "N/A","postal_code" => "75004","city" => "Paris", "external_id" => 3]
         ]);
         $ul_table->saveData();
 
         $queteur_table = $this->table('queteur');
         $queteur_table
-          ->addColumn('email'        , 'string', array('limit' => 100))     //non unique : parents may leave there email for child
-          ->addColumn('password'     , 'string', array('limit' => 50))
-          ->addColumn('password_salt', 'string', array('limit' => 50))
           ->addColumn('first_name'   , 'string', array('limit' => 50))
           ->addColumn('last_name'    , 'string', array('limit' => 50))
+          ->addColumn('email'        , 'string', array('limit' => 100))     //non unique : parents may leave there email for child
+          ->addColumn('password'     , 'string', array('limit' => 50, 'null' => true))
+          ->addColumn('password_salt', 'string', array('limit' => 50, 'null' => true))
           ->addColumn('secteur'      , 'integer')
           ->addColumn('nivol'        , 'string', array('limit' => 10))     //non unique : non volunteer won't have NIVOL, some volunteer may not know there NIVOL
           ->addColumn('mobile'       , 'string', array('limit' => 13))     //non unique : parents may leave there mobile for child
           ->addColumn('created'      , 'datetime')
           ->addColumn('updated'      , 'datetime', array('null' => true))
 
-          ->addColumn('parent_authorization'    , 'binary', array('null' => true))
-          ->addColumn('temporary_volunteer_form', 'binary', array('null' => true))
+          ->addColumn('parent_authorization'    , 'blob', array('null' => true))
+          ->addColumn('temporary_volunteer_form', 'blob', array('null' => true))
 
-          ->addColumn('notes', 'text')
+          ->addColumn('notes', 'text', array('null' => true))
           ->addColumn('ul_id', 'integer')
+
           ->addForeignKey('ul_id', 'ul', 'id')
           ->create();
 
@@ -70,46 +70,69 @@ class RedCrossQuest extends AbstractMigration
       $tronc_table = $this->table('tronc');
       $tronc_table
         ->addColumn('ul_id', 'integer')
+        ->addColumn('created'      , 'datetime')
         ->addForeignKey('ul_id', 'ul', 'id')
         ->create();
 
 
       $point_quete_table = $this->table('point_quete');
       $point_quete_table
-        ->addColumn('nom'         , 'string')
-        ->addColumn('longitude'   , 'string')
-        ->addColumn('latitude'    , 'string')
-        ->addColumn('adresse'     , 'string')
-        ->addColumn('codePostale' , 'string')
-        ->addColumn('ville'       , 'string')
-        ->addColumn('notes'       , 'text'  )
+        ->addColumn('ul_id'           , 'integer')
+        ->addColumn('code'            , 'string' , array('limit' => 10))
+        ->addColumn('nom'             , 'string' , array('limit' => 30))
+        ->addColumn('latitude'        , 'decimal', array('precision' => 10, 'scale' => 6))
+        ->addColumn('longitude'       , 'decimal', array('precision' => 10, 'scale' => 6))
+        ->addColumn('address'         , 'string' , array('limit' => 70))
+        ->addColumn('postal_code'     , 'string' , array('limit' => 15))
+        ->addColumn('city'            , 'string' , array('limit' => 70))
+        ->addColumn('max_people'      , 'text'  )
+        ->addColumn('advice'          , 'text'  , array('null' => true))
+        ->addColumn('localization'    , 'text'  )
+        ->addColumn('minor_allowed'   , 'boolean')
+        ->addColumn('created'         , 'datetime')
+
+        ->addForeignKey('ul_id', 'ul', 'id')
+
         ->create();
+
+
+
+
 
       $tronc_queteur_table = $this->table('tronc_queteur');
       $tronc_queteur_table
         ->addColumn('queteur_id'    , 'integer')
         ->addColumn('point_quete_id', 'integer')
+        ->addColumn('tronc_id'      , 'integer')
 
+        ->addColumn('depart_theorique', 'datetime')
         ->addColumn('depart'    , 'datetime')
         ->addColumn('retour'    , 'datetime')
-        ->addColumn('euro500'   , 'integer', 'nombre de billets de 500€')
-        ->addColumn('euro200'   , 'integer', 'nombre de billets de 200€')
-        ->addColumn('euro100'   , 'integer', 'nombre de billets de 100€')
-        ->addColumn('euro50'    , 'integer', 'nombre de billets de  50€')
-        ->addColumn('euro20'    , 'integer', 'nombre de billets de  20€')
-        ->addColumn('euro10'    , 'integer', 'nombre de billets de  10€')
-        ->addColumn('euro5'     , 'integer', 'nombre de billets de   5€')
-        ->addColumn('euro2'     , 'integer', 'nombre de pièces  de   2€')
-        ->addColumn('euro1'     , 'integer', 'nombre de pièces  de   1€')
-        ->addColumn('cents50'   , 'integer', 'nombre de pièces  de     50 cents')
-        ->addColumn('cents20'   , 'integer', 'nombre de pièces  de     20 cents')
-        ->addColumn('cents10'   , 'integer', 'nombre de pièces  de     10 cents')
-        ->addColumn('cents5'    , 'integer', 'nombre de pièces  de      5 cents')
-        ->addColumn('cents2'    , 'integer', 'nombre de pièces  de      2 cents')
-        ->addColumn('cent1'     , 'integer', 'nombre de pièces  de      1 cent ')
+
+        ->addColumn('euro500'   , 'integer', array('comment' => 'nombre de billets de 500€'))
+        ->addColumn('euro200'   , 'integer', array('comment' => 'nombre de billets de 200€'))
+        ->addColumn('euro100'   , 'integer', array('comment' => 'nombre de billets de 100€'))
+        ->addColumn('euro50'    , 'integer', array('comment' => 'nombre de billets de  50€'))
+        ->addColumn('euro20'    , 'integer', array('comment' => 'nombre de billets de  20€'))
+        ->addColumn('euro10'    , 'integer', array('comment' => 'nombre de billets de  10€'))
+        ->addColumn('euro5'     , 'integer', array('comment' => 'nombre de billets de   5€'))
+        ->addColumn('euro2'     , 'integer', array('comment' => 'nombre de pièces  de   2€'))
+        ->addColumn('euro1'     , 'integer', array('comment' => 'nombre de pièces  de   1€'))
+        ->addColumn('cents50'   , 'integer', array('comment' => 'nombre de pièces  de     50 cents'))
+        ->addColumn('cents20'   , 'integer', array('comment' => 'nombre de pièces  de     20 cents'))
+        ->addColumn('cents10'   , 'integer', array('comment' => 'nombre de pièces  de     10 cents'))
+        ->addColumn('cents5'    , 'integer', array('comment' => 'nombre de pièces  de      5 cents'))
+        ->addColumn('cents2'    , 'integer', array('comment' => 'nombre de pièces  de      2 cents'))
+        ->addColumn('cent1'     , 'integer', array('comment' => 'nombre de pièces  de      1 cent '))
+
+        ->addColumn('foreign_coins'    , 'integer', array('comment' => 'nombre de pièces  étrangères '))
+        ->addColumn('foreign_banknote' , 'integer', array('comment' => 'nombre de billets étranger'   ))
+
+        ->addColumn('notes', 'text', array('null' => true))
 
         ->addForeignKey('queteur_id'    , 'queteur'    , 'id')
         ->addForeignKey('point_quete_id', 'point_quete', 'id')
+        ->addForeignKey('tronc_id'      , 'tronc'      , 'id')
 
         ->create();
 
