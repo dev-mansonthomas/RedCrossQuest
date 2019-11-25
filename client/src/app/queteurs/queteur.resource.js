5df480b670b58934ce39e63fc574ebf1a785f073
Thomas Manson <dev.mansonthomas@gmail.com>
2019-11-25 23:34:05 +0100

    Remove action for update

--- client/src/app/queteurs/queteur.resource.js
+++ client/src/app/queteurs/queteur.resource.js
@@ -11,29 +11,25 @@ angular.module('redCrossQuestClient').factory('QueteurResource', function ($reso
       id: '@id'
     },
     {
-      update: {
+    update: {
+      method: 'PUT'
+    },
+    anonymize:
+      {
         method: 'PUT',
         params:
           {
-            action: 'update'
+            action: 'anonymize'
           }
       },
-      anonymize:
-        {
-          method: 'PUT',
-          params:
-            {
-              action: 'anonymize'
-            }
-        },
-      associateRegistrationWithExistingQueteur:
-       {
-         method: 'PUT',
-         params:
-           {
-             action: 'associateRegistrationWithExistingQueteur'
-           }
-       },
+    associateRegistrationWithExistingQueteur:
+     {
+       method: 'PUT',
+       params:
+         {
+           action: 'associateRegistrationWithExistingQueteur'
+         }
+     },
 
      markAllAsPrinted: {
         method: 'PUT',
@@ -42,50 +38,50 @@ angular.module('redCrossQuestClient').factory('QueteurResource', function ($reso
         }
       },
 
-      searchSimilarQueteurs:
-        {
-          method: 'GET',
-          isArray: true,
-          params:
-            {
-              action: 'searchSimilarQueteurs',
-              tronc_id: '@tronc_id'
-            }
-        },
-      getQueteurRegistration:
+    searchSimilarQueteurs:
+      {
+        method: 'GET',
+        isArray: true,
+        params:
+          {
+            action: 'searchSimilarQueteurs',
+            tronc_id: '@tronc_id'
+          }
+      },
+    getQueteurRegistration:
+    {
+      method: 'GET',
+      params:
+      {
+        action: 'getQueteurRegistration'
+      }
+    }
+    ,
+    countPendingQueteurRegistration:
       {
         method: 'GET',
         params:
-        {
-          action: 'getQueteurRegistration'
-        }
+          {
+            action: 'countPendingQueteurRegistration'
+          }
       }
-      ,
-      countPendingQueteurRegistration:
+    ,
+    listPendingQueteurRegistration:
+    {
+      method: 'GET',
+      isArray: true,
+      params:
         {
-          method: 'GET',
-          params:
-            {
-              action: 'countPendingQueteurRegistration'
-            }
+          action: 'listPendingQueteurRegistration'
         }
-      ,
-      listPendingQueteurRegistration:
-        {
-          method: 'GET',
-          isArray: true,
-          params:
-            {
-              action: 'listPendingQueteurRegistration'
-            }
-        },
-      approveQueteurRegistration:
-        {
-          method:'POST',
-          params:
-            {
-              action:'approveQueteurRegistration'
-            }
-        }
-    });
+    },
+    approveQueteurRegistration:
+      {
+        method:'POST',
+        params:
+          {
+            action:'approveQueteurRegistration'
+          }
+      }
+  });
 });
