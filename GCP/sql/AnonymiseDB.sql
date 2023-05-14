ee67bdae39274d7b1b3f294b520f7bcd4f8a8f01
Thomas Manson <dev.mansonthomas@gmail.com>
2023-05-14 12:47:48 +0200

    change default email address

--- GCP/sql/AnonymiseDB.sql
+++ GCP/sql/AnonymiseDB.sql
@@ -1,7 +1,7 @@
-update queteur        set email='rcq@mansonthomas.com' where id not in (select queteur_id from users);
+update queteur        set email='rcq@redcrossquest.com' where id not in (select queteur_id from users);
 update queteur        set mobile='33601020304';
 update users          set password = 'AAAAAAA'; -- password : disabled, users have to reinit it.
-update named_donation set email='rcq@mansonthomas.com';
+update named_donation set email='rcq@redcrossquest.com';
 update named_donation set phone='N/A';
 update named_donation set address='N/A';
 update named_donation set first_name='N/A';
